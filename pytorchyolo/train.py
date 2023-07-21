742048769dd2d295af8c5b79790e0143c8c545a6
Jafed Martinez <jafed.martinez@cimat.mx>
2023-07-21 14:30:08 -0600

    batch

--- pytorchyolo/train.py
+++ pytorchyolo/train.py
@@ -65,7 +65,7 @@ def run():
     parser.add_argument("-e", "--epochs", type=int, default=300, help="Number of epochs")
     parser.add_argument("-v", "--verbose", action='store_true', help="Makes the training more verbose")
     parser.add_argument("--n_cpu", type=int, default=8, help="Number of cpu threads to use during batch generation")
-    parser.add_argument("--pretrained_weights", type=str, default="weights/darknet53.conv.74", help="Path to checkpoint file (.weights or .pth). Starts training from checkpoint model")
+    parser.add_argument("-w", "--pretrained_weights", type=str, default="weights/darknet53.conv.74", help="Path to checkpoint file (.weights or .pth). Starts training from checkpoint model")
     parser.add_argument("--checkpoint_interval", type=int, default=1, help="Interval of epochs between saving model weights")
     parser.add_argument("--evaluation_interval", type=int, default=1, help="Interval of epochs between evaluations on validation set")
     parser.add_argument("--multiscale_training", action="store_true", help="Allow multi-scale training")
